0a7ec788c6352c300d6493d8c53d48888c59b798
Eugene Burtsev <eugene@burtsev.net>
2013-04-16 22:34:40 +0400

    Updated generate-index.rb script

--- generate-index.rb
+++ generate-index.rb
@@ -9,6 +9,7 @@ $html_template = <<END
 	<body>
 		<ul>
 			%s
+			%s
 		</ul>
 	</body>
 </html>
@@ -19,18 +20,21 @@ $link_template = "<li><a href='%1$s'>%1$s</a></li>"
 $base_dir = Dir.pwd
 
 def index_directory(dir, exclude_list = [])
-	links = ""
+	flinks = ""
+	dlinks = ""
 	Dir.foreach(dir) do |f|
 		if not exclude_list.include?(f)
 			path = File.join(dir, f)
-			links << $link_template % [f]
 			if File.directory?(path)
+				dlinks << $link_template % [f]
 				index_directory(path, [".", "..", "index.html"])
+			else
+				flinks << $link_template % [f]
 			end
 		end
 	end
 	index = open(File.join(dir, "index.html"), "w")
-	index.write($html_template % ["Index of #{dir.gsub($base_dir, "")}", links])
+	index.write($html_template % ["Index of #{dir.gsub($base_dir, "")}", dlinks, flinks])
 	index.close
 end
 
